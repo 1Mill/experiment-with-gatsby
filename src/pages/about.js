01837a2de36be7ec31696024caf14a213ba65855
1Mill <erik.ekberg@outlook.com>
2020-10-25 16:00:05 -0700

    Add link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 export default () => (
 	<main>
 		<h1>About me</h1>
 		<p>Welcome to my Gatsby site.</p>
+		<Link to='/'>Home page</Link>
 	</main>
 )
